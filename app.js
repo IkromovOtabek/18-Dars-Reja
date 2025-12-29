7f87f66e852e7ebf9b3760e137e0f507aac0f8f2
IkromovOtabek <otabekikromov32@gmail.com>
2025-12-29 18:53:10 +0500

    MongoDB ulanish

---
+++ app.js
@@ -0,0 +1,57 @@
+console.log('Web serverni boshlash');
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+// MongoDB chaqirish
+const db = require("./server").db();
+
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    }else{
+        user = JSON.parse(data)
+    }
+});
+
+// 1: Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// 2: Session code
+
+// 3 View code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 Routing code
+
+// app.get("/greeting", function(req, res){ //http://localhost:3000/greeting
+//     // res.end("Hello World Salom Otabek");
+//     res.end(`<h1 style="color: red">Hello Owen</h1>`);
+// });
+// app.get("/gift", function(req, res){ //http://localhost:3000/gift
+//     res.end(`<h1 style="color: green">Siz gift pagesizdasiz</h1>`);
+// });
+
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({ test: "success"});
+})
+
+app.get("/", function(req, res) {
+    res.render("reja");
+});
+
+app.get('/author', (req, res) => {
+    res.render("author", {user: user});
+})
+
+// express va ness eng  mashxur backend framework in Node.js
+
+// Frontendni 2xil usulda amalga oshiriladi: 1. Traditional - ejs framework foydalangan xolda BSSR - Backend Server Side Rendering - backendni ichida frontendi qurish 2. Single Page
+
+module.express = app;
--- server.js
+++ server.js
@@ -1,58 +1,23 @@
-console.log('Web serverni boshlash');
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err);
-    }else{
-        user = JSON.parse(data)
+let db;
+const connectionString = "mongodb+srv://Owen:6NxAfaczdNYWPKNu@owen.wkad1hh.mongodb.net/Reja"
+// mongodb+srv://user:password@cluster.mongodb.net/database?appName=Reja //16:26dan davom et
+
+mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true,}, (err, client) => {
+    if(err) console.log("ERROR on connection MongoDB");
+    else{
+        console.log("MngoDB connection succeed")
+
+        module.exports = client;
+        const app = require("./app");
+        const server = http.createServer(app);
+        let PORT = 3000;
+        server.listen(PORT, function(){
+            console.log(`The server is runing seccuessfully on port: ${PORT}, http://localhost:${PORT}`);
+        });
     }
 });
 
-// 1: Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-// 2: Session code
-
-// 3 View code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4 Routing code
-
-// app.get("/greeting", function(req, res){ //http://localhost:3000/greeting
-//     // res.end("Hello World Salom Otabek");
-//     res.end(`<h1 style="color: red">Hello Owen</h1>`);
-// });
-// app.get("/gift", function(req, res){ //http://localhost:3000/gift
-//     res.end(`<h1 style="color: green">Siz gift pagesizdasiz</h1>`);
-// });
-
-app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({ test: "success"});
-})
-
-app.get("/", function(req, res) {
-    res.render("reja");
-});
-
-app.get('/author', (req, res) => {
-    res.render("author", {user: user});
-})
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function(){
-    console.log(`The server is runing seccuessfully on port: ${PORT}, http://localhost:${PORT}`)
-});
-
-// express va ness eng  mashxur backend framework in Node.js
-
-// Frontendni 2xil usulda amalga oshiriladi: 1. Traditional - ejs framework foydalangan xolda BSSR - Backend Server Side Rendering - backendni ichida frontendi qurish 2. Single Page
+// mongodb+srv://Owen:6NxAfaczdNYWPKNu@owen.wkad1hh.mongodb.net/
